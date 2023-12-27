94cb8ef0bcf478bccc40e45c01e390c5b5399a98
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2023-12-27 23:36:55 +0100

    Create Concat_DIFF_SUM.py

---
+++ Concat_DIFF_SUM.py
@@ -0,0 +1,12 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+a, b = [int(i) for i in input().split()]
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print(str(a-b)+str(a+b))
