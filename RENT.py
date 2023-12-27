d65bb76a33b0fe5d5798a9e06452f51ba1ddb4fe
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2023-12-27 23:48:47 +0100

    Create RENT.py

---
+++ RENT.py
@@ -0,0 +1,17 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+if a * 6 * 5 * 4 - c - b >= 0 :
+    print("true")
+else : 
+    print("false")
